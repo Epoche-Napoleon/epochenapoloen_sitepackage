4970cc845cbdb332bbfd1d30cf722e94d40ac191
technik@epoche-napoleon.net <sarah70@mg041071A>
2021-04-07 00:53:51 +0200

    Add Additonal Layouts for headers

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,9 +9,9 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.17-9.5.99',
-            'Fsc' => '9.5.17-9.5.99',
-            'rte_ckeditor' => '9.17.0-9.5.99'
+            'typo3' => '9.5.25-9.5.99',
+            'fluid_styled_content' => '9.5.25-9.5.99',
+            'rte_ckeditor' => '9.25.0-9.5.99'
         ],
         'conflicts' => [
         ],
