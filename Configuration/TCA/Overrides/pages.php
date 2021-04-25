2abdad72f6f3672e582e81e42de82d0909b9f68a
technik@epoche-napoleon.net <sarah70@mg041071A>
2021-04-25 14:46:08 +0200

    Add http2-Header

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -6,3 +6,28 @@ defined('TYPO3_MODE') || die();
     '<INCLUDE_TYPOSCRIPT:source="FILE:EXT:'
     . $_EXTKEY .'/Configuration/PageTS/page.tsconfig">'
 );
+
+// Update für sourceIdentifier
+$tca = array(
+    'source_identifier' => array(
+        'exclude' => 1,
+        'label' => 'LLL:EXT:beaconizer/Resources/Private/Language/locallang_db.xlf:pages.source_identifier',
+        'config' => array(
+            'type' => 'input',
+            'size' => '30',
+            'max' => '40',
+            'eval' => 'nospace,unique',
+        )
+    ),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $tca);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'pages', // Table name
+    'source_identifier', // Field list to add
+    '', // List of specific types to add the field list to. (If empty, all type entries are affected)
+    'after:alias' // Insert fields before (default) or after one, or replace a field
+);
+
+$GLOBALS['TCA']['pages']['columns']['alias']['config']['eval'] = 'nospace,alphanum_x,unique';
