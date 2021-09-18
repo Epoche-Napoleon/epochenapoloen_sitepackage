714264fb9e548cd6954655eb4bba2cc4e8285b5e
Michael Gnessner <technik@epoche-napoleon.net>
2021-09-18 14:27:39 +0200

    Modify backend.css to custom backend.css for individual Backend Layout for Staging an Live
    Signed-off-by: Michael Gnessner <technik@epoche-napoleon.net>

--- ext_tables.php
+++ ext_tables.php
@@ -3,13 +3,9 @@
 
     $extensionKey = 'epochenapoleon_sitepackage';
 
-    $GLOBALS['TBE_STYLES']['skins']['backend'] = [
-        'name' => 'epochenapoleon',
-    ];
-
     if (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext() == "Development") {
-        $GLOBALS['TBE_STYLES']['skins']['epochenapoleon'] ['epochenapoleon']['stylesheetDirectories']['live'] = 'EXT:'.$extensionKey. '/Resources/Public/Backend/Stage/Css/Skin';
+        $GLOBALS['TBE_STYLES']['skins']['backend'] ['epochenapoleon']['stylesheetDirectories']['live'] = 'EXT:'.$extensionKey. '/Resources/Public/Backend/Stage/Css/Skin';
     }
     else {
-        $GLOBALS['TBE_STYLES']['skins']['epochenapoleon'][stylesheetDirectories]['live'] = 'EXT:'.$extensionKey.'/Resources/Public/Backend/Live/Css/Skin';
+        $GLOBALS['TBE_STYLES']['skins']['backend'][stylesheetDirectories]['live'] = 'EXT:'.$extensionKey.'/Resources/Public/Backend/Live/Css/Skin';
     }
