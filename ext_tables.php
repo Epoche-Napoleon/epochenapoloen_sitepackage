20d0356ad137828e353764391a1e21effcc41b7b
Michael Gnessner <technik@epoche-napoleon.net>
2021-09-25 11:27:57 +0200

    Application Context use in Backend
    Signed-off-by: Michael Gnessner <technik@epoche-napoleon.net>

--- ext_tables.php
+++ ext_tables.php
@@ -4,7 +4,7 @@
 
     if (!\TYPO3\CMS\Core\Core\Environment::getContext()->isProduction()
 ) {
-    $GLOBALS['TBE_STYLES']['skins']['epochenapoleon_sitpackage'] = [
+    $GLOBALS['TBE_STYLES']['skins']['epochenapoleon-sitpackage'] = [
         'name' => 'Development',
         'stylesheetDirectories' => [
             'css' => 'EXT:epochenapoleon_sitepackage/Resources/Public/Css/backend.css'
