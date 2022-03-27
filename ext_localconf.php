849491b16bcb51b2c7d514352667e2e9dbda4545
Epoche-Napoleon <technik@epoche-napoleon.net>
2022-03-27 11:28:36 +0200

    Update $GLOBALS['TCA']['tx_news_domain_model_news']['ctrl']['thumbnail'] = 'fal_media'; for show thumbnails in tx_news
    Signed-off-by: Epoche-Napoleon <technik@epoche-napoleon.net>

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,4 +7,4 @@ defined('TYPO3_MODE') || die();
 // Individual RTE Configuration
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['epochenapoleon_preset'] = 'EXT:epochenapoleon_sitepackage/Configuration/RTE/Yaml/epochenapoleon_preset.yaml';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['EventLinkViewHelper'] = ['epochenapoleon\\epochjenapoleon_sitepackage\\ViewHelpers'];
-
+$GLOBALS['TCA']['tx_news_domain_model_news']['ctrl']['thumbnail'] = 'fal_media';
