d8d7d0dc6d5079785ea9862494aa4c93be74b1f6
Epoche-Napoleon <technik@epoche-napoleon.net>
2022-05-21 09:32:52 +0200

    Update $GLOBALS['TCA']['pages']['ctrl']['thumbnail'] = 'fal_media'; for show thumbnails in tx_news
    Signed-off-by: Epoche-Napoleon <technik@epoche-napoleon.net>

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -6,3 +6,5 @@ defined('TYPO3_MODE') || die();
     '<INCLUDE_TYPOSCRIPT:source="FILE:EXT:'
     . $_EXTKEY .'/Configuration/PageTS/page.tsconfig">'
 );
+# Mit TYPO3 11 zu entfernen oder EXT:studiomitte/recordlist-thumbnail installieren
+$GLOBALS['TCA']['pages']['ctrl']['thumbnail'] = 'fal_media';
--- Configuration/TCA/Overrides/tx_news_domain_model_news.php
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -2,4 +2,6 @@
 
 defined('TYPO3_MODE') || die();
 
+# Mit TYPO3 11 zu entfernen oder EXT:studiomitte/recordlist-thumbnail installieren
+
 $GLOBALS['TCA']['tx_news_domain_model_news']['ctrl']['thumbnail'] = 'fal_media';
