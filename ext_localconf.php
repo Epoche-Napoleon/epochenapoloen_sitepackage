3eb89ac295de129263172aa1a8264e86102fda29
Epoche-Napoleon <technik@epoche-napoleon.net>
2022-12-11 13:52:55 +0100

    Add print.css
    Signed-off-by: Epoche-Napoleon <technik@epoche-napoleon.net>

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,3 +8,13 @@ defined('TYPO3_MODE') || die();
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['epochenapoleon_preset'] = 'EXT:epochenapoleon_sitepackage/Configuration/RTE/Yaml/epochenapoleon_preset.yaml';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['EventLinkViewHelper'] = ['epochenapoleon\\epochjenapoleon_sitepackage\\ViewHelpers'];
 $GLOBALS['TCA']['tx_news_domain_model_news']['ctrl']['thumbnail'] = 'fal_media';
+
+// Erweiterung Glossary2
+// $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['glossary2']['extender']['CLASSNAME']['epochenapoleon_sitepackage'] =
+    \Vendor\EXTKEY2\Domain\Model\CLASSNAME::class;
+
+// $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['glossary2']['extender']['CLASSNAME']['epochenapoleon_sitepackage'] =
+    'EXT:EXTKEY2/Classes/Domain/Model/CLASSNAME.php';
+
+// $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['glossary2']['extender']['CLASSNAME']['epochenapoleon_sitepackage'] =
+    'Domain/Model/SingleModel';
